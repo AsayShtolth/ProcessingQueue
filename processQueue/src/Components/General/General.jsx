8ae6a4f6e4fc33eb544abd04a413846def3872c0
AsayShtolth <ri1515@yandex.ru>
2024-05-02 16:48:35 +0300

    Add save and get General Settings

--- processQueue/src/Components/General/General.jsx
+++ processQueue/src/Components/General/General.jsx
@@ -1,20 +1,48 @@
 import  { useState } from "react";
 import { RadioButton } from "primereact/radiobutton";
 import { Button } from 'primereact/button';
+const saveChanges=(ingredient, time)=>{
+		fetch('https://check.detexline.ru/processingqueue/api/saveGeneralSettings.php', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ 'areaView': ingredient, 'timeView': time })
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error))
+	}
 const General = () => {
-	const [ingredient, setIngredient] = useState('');
-	const [time, setTime] = useState(1);
+	const [ingredient, setIngredient] = useState();
+	const [time, setTime] = useState();
+	fetch('https://check.detexline.ru/processingqueue/api/getGeneralSettings.php', {
+  method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+//   body: JSON.stringify({ 'areaView': ingredient, 'timeView': time })
+})
+  .then(response => response.json())
+  .then(data => {
+	JSON.stringify(data);
+	console.log(data);
+	setIngredient(data[0]);
+	setTime(data[1]);
+  })
+  .catch(error => console.error(error))
+
 	return ( 
 		<>
 		<div className="card flex justify-content-center">
             <div className="flex flex-wrap gap-3">
 				<span className="font-bold">Отображение</span>
                 <div className="flex align-items-center">
-                    <RadioButton inputId="ingredient1" name="numAreas" value="Cheese" onChange={(e) => setIngredient(e.value)} checked={ingredient === 'Cheese'} />
+                    <RadioButton inputId="ingredient1" name="numAreas" value={1} onChange={(e) => setIngredient(e.value)} checked={ingredient == 1} />
                     <label htmlFor="ingredient1" className="ml-2">1 участок</label>
                 </div>
                 <div className="flex align-items-center">
-                    <RadioButton inputId="ingredient2" name="numAreas" value="Mushroom" onChange={(e) => setIngredient(e.value)} checked={ingredient === 'Mushroom'} />
+                    <RadioButton inputId="ingredient2" name="numAreas" value={2} onChange={(e) => setIngredient(e.value)} checked={ingredient == 2} />
                     <label htmlFor="ingredient2" className="ml-2">2 участка</label>
                 </div>
             </div>
@@ -23,20 +51,20 @@ const General = () => {
             <div className="flex flex-wrap gap-3">
 				<span className="font-bold">Время смены участков</span>
                 <div className="flex align-items-center">
-                    <RadioButton inputId="ingredient1" name="numAreas" value="1" onChange={(e) => setTime(e.value)} checked={time == 1} />
+                    <RadioButton inputId="ingredient1" name="numAreas" value={1} onChange={(e) => setTime(e.value)} checked={time == 1} />
                     <label htmlFor="ingredient1" className="ml-2">1 минута</label>
                 </div>
                 <div className="flex align-items-center">
-                    <RadioButton inputId="ingredient2" name="numAreas" value="3" onChange={(e) => setTime(e.value)} checked={time == 3} />
+                    <RadioButton inputId="ingredient2" name="numAreas" value={3} onChange={(e) => setTime(e.value)} checked={time == 3} />
                     <label htmlFor="ingredient2" className="ml-2">3 минуты</label>
                 </div>
 				<div className="flex align-items-center">
-                    <RadioButton inputId="ingredient2" name="numAreas" value="6" onChange={(e) => setTime(e.value)} checked={time == 6} />
+                    <RadioButton inputId="ingredient2" name="numAreas" value={6} onChange={(e) => setTime(e.value)} checked={time == 6} />
                     <label htmlFor="ingredient2" className="ml-2">6 минут</label>
                 </div>
             </div>
         </div>
-		<Button label="Сохранить изменения" rounded />
+		<Button label="Сохранить изменения" rounded onClick={saveChanges(ingredient, time)}/>
 		</>
 	);
 }
